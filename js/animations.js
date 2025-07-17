ca48228af77c9cb28ffd9fce9c5eccc245395fe9
Maryna K <maryna@dunaisky.com>
2025-07-17 19:52:07 +0300

    improvements + added innovations block

--- js/animations.js
+++ js/animations.js
@@ -15,11 +15,13 @@ const observer = new IntersectionObserver(
 
 elements.forEach((el) => observer.observe(el));
 
+// PARALLAX
+
 const hero = document.querySelector(".hero");
 
 hero.addEventListener("mousemove", (e) => {
-  const x = e.clientX / window.innerWidth - 0.5;
+  // const x = e.clientX / window.innerWidth - 0.5;
   const y = e.clientY / window.innerHeight - 0.5;
-  hero.style.setProperty("--parallax-x", `${x * 20}px`);
+  // hero.style.setProperty("--parallax-x", `${x * 5}px`);
   hero.style.setProperty("--parallax-y", `${y * 20}px`);
 });
